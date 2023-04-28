ddd83d84cdcf4fccd39eb745a8c7457f5702b085
Marley Alford <marleycalford@gmail.com>
2023-04-28 18:29:26 +0200

    WIP - creating resume items

---
+++ src/components/ResumeItem.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="resumeItem">
+    <div class="heading">
+      <h2>title</h2>
+      <p>location</p>
+      <p>date</p>
+    </div>
+    <ul>
+      <li>thing 1</li>
+      <li>thing 2</li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+@media (min-width: 1024px) {
+  h2 {
+    font-size: large;
+  }
+  p {
+    font-size: medium;
+  }
+  .heading {
+    display: flex;
+    justify-content: space-between;
+  }
+}
+</style>
--- src/views/TechView.vue
+++ src/views/TechView.vue
@@ -1,17 +1,30 @@
 <script lang="ts">
 import TabView from './TabView.vue'
+import ResumeItem from '../components/ResumeItem.vue'
 export default {
-  components: { TabView }
+  components: { ResumeItem, TabView }
 }
 </script>
 <template>
   <TabView>
-    <h1>Resume</h1>
-    <h2>Summary</h2>
-    <h2>Skills</h2>
-    <h2>Work Experience</h2>
-    <h2>Education</h2>
-    <h2>Honors</h2>
+    <div class="resume-container">
+      <h1>Summary</h1>
+      <p>
+        Frontend engineer with 4 years of professional experience across the stack. Alum of Bard
+        College '17, hackNY '17, and StartupBus '22. NY -> Berlin.
+      </p>
+      <h1>Skills</h1>
+      <p>
+        Javascript, Typescript, HTML, CSS, React, Vue, NextJs, D3, Cypress, Storybook, CircleCI,
+        PostgresQL.
+      </p>
+      <h1>Experience</h1>
+      <ResumeItem />
+      <ResumeItem />
+
+      <h2>Education</h2>
+      <h2>Honors</h2>
+    </div>
   </TabView>
 </template>
 
@@ -22,6 +35,10 @@ export default {
   }
   h2 {
     font-size: large;
+  }
+  .resume-container {
+    height: 100%;
+    width: 90%;
   }
 }
 </style>
